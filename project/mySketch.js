b0af4e0a8c3b2d8426f608ac5023000f009814ab
gozanne <25gavin.ozanne@lwhs.org>
2025-04-03 22:29:53 +0000

    first commit

--- project/mySketch.js
+++ project/mySketch.js
@@ -1,10 +1,20 @@
+let data
+
+function preload() {
+  data = loadJSON("flower.json")
+
+}
 
 function setup() {
   createCanvas(400, 400);
   background(200);
+  console.log(data.fruits)
  
 }
 
 function draw(){
-  
+  for (i in data.fruits) {
+    text(data.fruits[i], 10, 10 + 10*i)
+  }
+
 }

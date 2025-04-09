c2ed65aee663c2aa71c3c3756b13577372fa3664
gozanne <25gavin.ozanne@lwhs.org>
2025-04-09 18:21:42 +0000

    new UI

--- project/mySketch.js
+++ project/mySketch.js
@@ -1,26 +1,51 @@
-var weather;
+let weather0;
+let weather1;
+let weather2;
+let weather3;
+
+function preload() {
+  weather0 = loadJSON('https://api.openweathermap.org/data/2.5/weather?id=5128581&APPID=001b0f58045147663b1ea518d34d88b4')
+  weather1 = loadJSON('https://api.openweathermap.org/data/2.5/weather?id=5391997&APPID=001b0f58045147663b1ea518d34d88b4')
+
+}
 
 function setup() {
-  createCanvas(400, 200);
-  loadJSON(
-    'https://api.openweathermap.org/data/2.5/weather?id=5391997&APPID=001b0f58045147663b1ea518d34d88b4',
-    gotData
-  );
+  createCanvas(800, 800);
+  textAlign(CENTER, CENTER)
 }
 
-function gotData(data) {
-  weather = data;
-}
+
 
 function draw() {
-  background(0);
-  if (weather) {
-    fill(255);
-    ellipse(100, 100, weather.main.temp, weather.main.temp);
-    ellipse(300, 100, weather.main.humidity, weather.main.humidity);
+  background(200);
+  drawUI(weather0, 0, 0)
+  drawUI(weather1, 1, 0)
+}
 
-    fill('red')
-    text(weather.main.temp, 100,100);
-    text(weather.main.humidity, 300,100);
-  }
+function drawUI(info, column, row) {
+
+  // Setup
+  stroke(150)
+  fill(150)
+  rectMode(CORNER)
+  rect((column * 400) + 10, (row * 400) + 10, 380, 380)
+
+  //Title
+  stroke(100)
+  fill(100)
+  rectMode(CENTER)
+  rect((column * 400) + 200, (row * 400) + 40, textWidth(info.name) + 20, 40, 10)
+
+  stroke(255)
+  fill(255)
+  textAlign(CENTER, CENTER)
+  textSize(26)
+  text(info.name, (column * 400) + 200, (row * 400) + 40)
+
+  //Temp
+
+
+
+
+
 }
